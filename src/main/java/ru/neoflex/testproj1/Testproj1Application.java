a145daf867110296d1933dd1f5fd4d70393f278e
Iaroslav Roshchupkin <iroshchupkin@neoflex.ru>
2024-12-20 14:43:43 +0300

    init

---
+++ src/main/java/ru/neoflex/testproj1/Testproj1Application.java
@@ -0,0 +1,13 @@
+package ru.neoflex.testproj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testproj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testproj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/neoflex/testproj1/Testproj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.testproj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testproj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
